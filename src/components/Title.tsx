1b31ea2f7764fb1046edff131665a5f789772a91
Kane <LisaxKane@gmail.com>
2023-02-25 23:37:12 -0600

    added extra container for arrow to be responsive

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -50,16 +50,18 @@ export default function Title() {
             A WEB <span id='dev'>DEV</span>
           </div>
         </div>
-        <div id='description'>
-          {" "}
-          I'VE BEEN PROGRAMMING FOR 4 YEARS WITH 1 YEAR IN WEB DEV. LETS
-          CONNECT!
+        <div id='description-container'>
+          <div id='description'>
+            {" "}
+            I'VE BEEN PROGRAMMING FOR 4 YEARS WITH 1 YEAR IN WEB DEV. LETS
+            CONNECT!
+          </div>
         </div>
-      </div>
-      <img src={arrow} alt='arrow' id='arrow' />
-      <div className='button-container'>
-        <span id='project-btn'>projects</span>
-        <span id='connect-btn'>connect</span>
+        <img src={arrow} alt='arrow' id='arrow' />
+        <div className='button-container'>
+          <span id='project-btn'>projects</span>
+          <span id='connect-btn'>connect</span>
+        </div>
       </div>
     </div>
   );
